e65725df10d1b0cbb26862c6ece3be257290e755
apariciozuniga <bereniceaparicio31@gmail.com>
2016-11-18 17:39:39 +0000

    agregando pagina

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('ul').click(function(){
+		$(this).slideUp(3000);
+	});
+	$('ul').click(function(){
+		$(this).slideDown(3000);
+	});
+	$('ul').click(function(){
+		$(this).slideUp(3000);
+	});
+	
+
+});
